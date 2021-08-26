60c7875b58da2c8471657be88d36e382e6e0384b
shimys65 <shimys64@hanmail.net>
2021-08-26 13:16:17 +0900

    게시물 찾기, 페이징

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -16,7 +16,7 @@ fun main() {
         val rq = Rq(command)
 
         when (rq.actionPath) {
-            "/system/init" -> {
+            "/system/exit" -> {
                 println("시스템 종료")
                 break
             }
@@ -27,9 +27,68 @@ fun main() {
                     println("id를 입력해 주세요.")
                     continue
                 }
-                val article = articleRepository.articles[id-1]
-                println(article)
-            }
+                val article = articleRepository.getArticleByID(id)
+
+                if (article == null) {
+                    println("${id}번 게시물은 존재하지 않습니다.")
+                    continue
+                }
+                println("번호 : ${article.id}")
+                println("작성날짜 : ${article.regDate}")
+                println("갱신날짜 : ${article.updateDate}")
+                println("제목 : ${article.title}")
+                println("내용 : ${article.body}")
+            } // end of "/article/detail"
+
+            "/article/modify" -> {
+                val id = rq.getIntParam("id", 0)
+
+                if (id == 0){
+                    println("id를 입력해 주세요.")
+                    continue
+                }
+                val article = articleRepository.getArticleByID(id)
+
+                if (article == null) {
+                    println("${id}번 게시물은 존재하지 않습니다.")
+                    continue
+                }
+                print("${id}번 게시물 새 제목 : ")
+                val title = readLineTrim()
+                print("${id}번 게시물 새 제목 : ")
+                val body = readLineTrim()
+
+                articleRepository.articleModify(id, title, body)
+                println("${id}번 게시물이 수정되었음")
+            } // end of "/article/modify"
+
+            "/article/list" -> {
+                val page = rq.getIntParam("page", 1) //입력 명령어가 page
+                val searchKeyword = rq.getStringParam("searchKeyword", "") //입력 명령어가 searchKeyword
+
+                val filteredArticles = articleRepository.getFilteredArticles(searchKeyword, page,10)
+
+                println("번호 / 작성날짜 / 갱신날짜 / 제목 / 내용")
+                for (article in filteredArticles) {
+                    println("${article.id} / ${article.regDate} / ${article.updateDate} / ${article.title} / ${article.body}")
+                }
+            } // end of "/article/list"
+
+            "/article/delete" -> {
+                val id = rq.getIntParam("id", 0)
+
+                if (id == 0) {
+                    println("id를 입력해 주세요.")
+                    continue
+                }
+                val article = articleRepository.getArticleByID(id)
+
+                if (article == null) {
+                    println("${id}번 게시물은 존재하지 않습니다.")
+                    continue
+                }
+                articleRepository.deleteArticle(article)
+            } // end of "/article/delete"
         }
     }
 
@@ -37,27 +96,96 @@ fun main() {
 }
 
 data class Article(
-    val id: Int, val regDate: String, val updateDate: String, val title: String, val body: String,
+    val id: Int, val regDate: String, var updateDate: String, var title: String, var body: String,
 )
+
 
 object articleRepository {
     val articles = mutableListOf<Article>()
     var lastId = 0
+
+    fun getArticleByID(id: Int) : Article? {
+        for (article in articles) {
+            if (article.id == id) {
+                return article
+            }
+        }
+        return  null
+    }
 
     fun addArticle(title: String, body: String) {
         val id = ++lastId
         val regDate = Util.getNowDateStr()
         val updateDate = Util.getNowDateStr()
 
-        articles.add(Article(id, regDate, updateDate, title, body ))
+        articles.add(Article(id, regDate, updateDate, title, body))
     }
 
     fun makeTestArticle() {
-        for (id in 0..100) {
-            addArticle("제목_$id", "내용_$id" )
+        for (id in 1..100) {
+            addArticle("제목_$id", "내용_$id")
         }
     }
-}
+
+    fun deleteArticle(article: Article) {
+        articles.remove(article)
+    }
+
+     @JvmName("getArticles1")
+     fun getArticles(): List<Article> { // mutableListOf가 아닌 List로 하면 권한이 축소, 추가가 안됨.
+        return articles
+    }
+
+    fun articleModify(id: Int, title: String, body: String) {
+        val article = getArticleByID(id)!!
+
+        article.title = title
+        article.body = body
+        article.updateDate = Util.getNowDateStr()
+    }
+
+    fun getFilteredArticles(searchKeyword: String, page: Int, itemsCountInAPage: Int): List<Article> {
+        // 1차 searchKeyword로 필터돤 것을 저장
+        val filtered1Articles = getSearchKeywordFilteredArticles(articles, searchKeyword)
+        // 2차 page로 필터돤 것을 저장
+        val filtered2Articles = getPageFilteredArticles(filtered1Articles, page, itemsCountInAPage)
+
+        return filtered2Articles
+    }
+
+
+    private fun getSearchKeywordFilteredArticles(articles: List<Article>, searchKeyword: String): List<Article> {
+        val filteredArticles = mutableListOf<Article>()
+
+        for (article in articles) {
+            if (article.title.contains(searchKeyword)) {
+                filteredArticles.add(article)
+            }
+        }
+        return filteredArticles
+    }
+
+
+    private fun getPageFilteredArticles(filtered1Articles: List<Article>, page: Int, itemsCountInAPage: Int): List<Article> {
+        val filteredArticles = mutableListOf<Article>()
+
+        val offsetCount = (page - 1) * itemsCountInAPage
+
+        val startIndex = articles.lastIndex - offsetCount
+        var endIndex = startIndex - (itemsCountInAPage - 1)
+
+        if (endIndex < 0) {
+            endIndex = 0
+        }
+
+        for (i in startIndex downTo endIndex) {
+            filteredArticles.add(articles[i])
+        }
+
+        return filteredArticles
+    }
+
+} // end of articleRepository
 
 class Rq(command: String) {
     // 전체 URL : /artile/detail?id=1
